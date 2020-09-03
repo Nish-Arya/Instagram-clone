07d28eaff70df0a403abead838147a905b623406
Nishchay Arya <nishchay.arya92@gmail.com>
2020-09-03 11:42:27 -0400

    Style navbar

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -23,8 +23,8 @@ function HomePage() {
       <div className="home__header">
         <div className="home__navbar">
           <img className="navbar__logo" src={logo} alt="logo" />
-          <HomeIcon style={{ fontSize: 30 }} className='navbar__button' type='submit' />
-          <button className='navbar__button' onClick={handleClick} type='submit'>Log Out</button>
+          <HomeIcon style={{ fontSize: 30 }} className='navbar__button__home' type='submit' />
+          <button className='navbar__button__logout' onClick={handleClick} type='submit'>Log Out</button>
         </div>
       </div>
       <Post />
