36c2841f777dbadb1c0e7c6b08606836a1d6ba85
Nish-Arya <nishchay.arya92@gmail.com>
2021-03-25 20:24:04 -0400

    SSL fix

--- client/src/store/posts.js
+++ client/src/store/posts.js
@@ -1,7 +1,7 @@
 // import Cookies from 'js-cookie';
 
 const SET_POSTS = 'posts/SET_POSTS';
-
+const POST_COMMENT = "posts/POST_COMMENT";
 
 export const setPosts = posts => {
   return {
@@ -9,6 +9,32 @@ export const setPosts = posts => {
     posts
   }
 }
+
+export const postComment = (comment) => {
+  return {
+    type: POST_COMMENT,
+    comment,
+  };
+};
+
+export const sendComment = (postId, userId, username, body) => {
+  return async (dispatch) => {
+    const res = await fetch(`/api/posts/${postId}/comments`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        "XSRF-TOKEN": Cookies.get("XSRF-TOKEN"),
+      },
+      body: JSON.stringify({ postId, userId, body, username }),
+    });
+    res.data = await res.json();
+    if (res.ok) {
+      console.log(res.data.comment);
+      dispatch(postComment(res.data.comment));
+    }
+    return res;
+  };
+};
 
 export const getPosts = () => {
   return async dispatch => {
@@ -25,6 +51,8 @@ export default function postsReducer(state={}, action) {
   switch (action.type) {
     case SET_POSTS:
       return action.posts;
+    case POST_COMMENT:
+      return state;
     default:
       return state;
   }
